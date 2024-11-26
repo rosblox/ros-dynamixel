21c4824dfb4e6e36079fbf4f5bff8f6b8043d002
Max Polzin <polzin.max@gmail.com>
2024-11-26 11:25:05 +0100

    Update volumes.

--- run.sh
+++ run.sh
@@ -7,6 +7,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 export HOST_UID=$(id -u)
 
 docker compose -f $SCRIPT_DIR/docker-compose.yml run \
---volume $(pwd)/livox_ros_driver2:/colcon_ws/src/livox_ros_driver2 \
---volume $(pwd)/Livox-SDK2:/Livox-SDK2 \
+--volume $(pwd)/DynamixelSDK/dynamixel_sdk_examples:/colcon_ws/src/dynamixel_sdk_examples \
+--volume $(pwd)/DynamixelSDK/dynamixel_sdk_custom_interfaces:/colcon_ws/src/dynamixel_sdk_custom_interfaces \
 ${REPOSITORY_NAME} bash
